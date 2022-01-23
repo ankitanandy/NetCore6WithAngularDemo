e813f5f33cd778b5513c6061ebc1312e5b256fa1
Ankita <nandy.ankita@gmail.com>
2022-01-23 16:20:32 +0000

    initial commit 3

--- TestAPI/Controllers/EmployeeController.cs
+++ TestAPI/Controllers/EmployeeController.cs
@@ -20,4 +20,18 @@ public class EmployeeController : ControllerBase
     {
         return new JsonResult(await this.context.Employees.ToListAsync());
     }
+
+   [HttpGet("{id}")]
+    public async Task<ActionResult<List<Department>>> Get(int id)
+    {
+        var employee = await context.Employees.FindAsync(id);
+        return Ok(employee);
+    }
+
+    // [HttpPost(Name = "AddEmployee")]
+    // public async Task<JsonResult> Add()
+    // {
+    //     context.Employees.Add()
+    //     return new JsonResult(await this.context.Employees.ToListAsync());
+    // }
 }
